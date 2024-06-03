90ccf5b56b7dcc5b326c2256553902da7c102648
Himmat0508 <nathawathimmatsingh0508@gmail.com>
2024-06-03 20:38:50 +0530

    Combining four commit in single commit

--- src/test/java/com/NewMavenProject/FirstMavenProject/TestNGDemo.java
+++ src/test/java/com/NewMavenProject/FirstMavenProject/TestNGDemo.java
@@ -1,5 +1,26 @@
 package com.NewMavenProject.FirstMavenProject;
 
 public class TestNGDemo {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Firat Commit");
+		
+		System.out.println("Second Commit");
+		
+		System.out.println("Thired Commit");
+		
+		System.out.println("four Commit");
+		
+		System.out.println("Five Commit");
+
+		
+		
+
+
+
+	}
 
 }
