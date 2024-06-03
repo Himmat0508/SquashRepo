903d4503baf2a6d1a1263f3361b23eb1ae4c356b
Himmat0508 <nathawathimmatsingh0508@gmail.com>
2024-06-03 20:34:09 +0530

    Commited by NewUser

---
+++ src/test/java/com/NewMavenProject/FirstMavenProject/TestNGDemo.java
@@ -0,0 +1,5 @@
+package com.NewMavenProject.FirstMavenProject;
+
+public class TestNGDemo {
+
+}
---
+++ src/test/java/org/openqa/selenium/WebDriver.java
@@ -0,0 +1,8 @@
+package org.openqa.selenium;
+
+import org.testng.annotations.Test;
+
+@Test
+public class WebDriver {
+
+}
